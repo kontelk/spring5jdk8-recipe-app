c9ae380025279de18e63b3aa571968af5ff125a4
kontelk <ktelkis@gmail.com>
2023-08-20 17:49:09 +0300

    Initial commit

---
+++ src/main/java/com/kt/spring5jdk8recipeapp/Spring5jdk8RecipeAppApplication.java
@@ -0,0 +1,13 @@
+package com.kt.spring5jdk8recipeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5jdk8RecipeAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring5jdk8RecipeAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kt/spring5jdk8recipeapp/Spring5jdk8RecipeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kt.spring5jdk8recipeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5jdk8RecipeAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
